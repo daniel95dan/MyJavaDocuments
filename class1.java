b1144f18acc6ed39dfa880adaff29c4b1592cf46
Daniel <daniel_iancu96@yahoo.com>
2018-05-11 20:38:50 +0300

    Second commit

--- class1.java
+++ class1.java
@@ -1,7 +1,7 @@
 public class class1
 {
-public static void main(String [] args)
-{
-System.out.print("Hello world!");
-}
+	public static void main(String [] args)
+	{
+		System.out.print("Hello world!");
+	}
 }
